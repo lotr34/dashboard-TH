1d562d0b1020bff24fd958c78fc5d4cd262cebaa
lotr34 <halimdalgin@hotmail.com>
2022-08-16 09:54:53 +0300

    deploy

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div className='bg-[#040816] w-full h-screen overflow-scroll md:scrollbar-hide'>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
